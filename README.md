c51e7a8969b2415bd6fd8b5369f1e072d5ac5fed
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-08-31 00:20:23 +0700

    Give mob programming a try

---
+++ README.md
@@ -0,0 +1,1 @@
+Read problem.txt file and use data.txt in resource folder to solve the problem
---
+++ src/main/java/com/example/mobprogramming/MobProgrammingApplication.java
@@ -0,0 +1,12 @@
+package com.example.mobprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobProgrammingApplication.class, args);
+    }
+}
